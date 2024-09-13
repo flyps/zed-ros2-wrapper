1abf98cc64717bb5855b6087b5cbe2c457f28c86
Kajetan Sulwiński <kajo.sulwinski@gmail.com>
2024-09-13 13:47:02 +0200

    fix: launch file, add: docker compose to start stream and recording

--- zed_wrapper/launch/stream_topics.launch.py
+++ zed_wrapper/launch/stream_topics.launch.py
@@ -1,8 +1,7 @@
 from launch import LaunchDescription
-from launch.actions import IncludeLaunchDescription
+from launch.actions import IncludeLaunchDescription, DeclareLaunchArgument
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch_ros.substitutions import FindPackageShare
-from launch.actions import DeclareLaunchArgument
 from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
 import os
 
@@ -11,31 +10,35 @@ from launch_ros.actions import Node
 
 def generate_launch_description():
     # Declare launch arguments
-    DeclareLaunchArgument("camera_config", default_value="zedx.yaml")
-
-    # start the ZED X
-    zed_wrapper_path = FindPackageShare(package="zed_wrapper").find("zed_wrapper")
-    camera_config = LaunchConfiguration("camera_config")
-    ros_params_override_path = PathJoinSubstitution(
-        [zed_wrapper_path, "config", camera_config]
+    camera_config = LaunchConfiguration('camera_config')
+    camera_config_launch_arg = DeclareLaunchArgument(
+        'camera_config',
+        default_value='zedx.yaml',
+        description='Path to the camera configuration file, relative to config/ directory'
     )
+
+    # Start the ZED X node
+    zed_wrapper_path = FindPackageShare(package='zed_wrapper').find('zed_wrapper')
+    ros_params_override_path = PathJoinSubstitution([zed_wrapper_path, 'config', camera_config])
     start_zedx = IncludeLaunchDescription(
         PythonLaunchDescriptionSource(
-            os.path.join(zed_wrapper_path, "launch", "zed_camera.launch.py")
-        ),
+            os.path.join(zed_wrapper_path, "launch", "zed_camera.launch.py")),
         launch_arguments={
-            "camera_model": "zedx",
-            "ros_params_override_path": ros_params_override_path,
-        }.items(),
+            'camera_model': 'zedx',
+            'ros_params_override_path': ros_params_override_path
+        }.items()
     )
 
-    # start web video server
-    # ros2 run web_video_server web_video_server
+    # Start web video server node
     start_web_video_server = Node(
-        package="web_video_server",
-        executable="web_video_server",
-        name="web_video_server",
-        output="screen",
+        package='web_video_server',
+        executable='web_video_server',
+        name='web_video_server',
+        output='screen'
     )
 
-    return LaunchDescription([start_zedx, start_web_video_server])
+    return LaunchDescription([
+        camera_config_launch_arg,  
+        start_zedx,
+        start_web_video_server
+    ])
